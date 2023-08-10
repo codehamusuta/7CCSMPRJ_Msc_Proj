f1c32b08c8116898969d90611d89218e33aa1260
codehamusuta <codehamusuta>
2023-08-10 19:15:01 +0100

    added module comments

--- src/data_processing/process_climate.py
+++ src/data_processing/process_climate.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+# -----------------------
+# Author : Wen Ting, Choi
+# Date : 2023-08-09 
+# -----------------------
+"""Data processing script for CLIMATE-FEVER data
+"""
+
 import os
 import json
 
---
+++ src/data_processing/process_pubhealth.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# -----------------------
+# Author : Wen Ting, Choi
+# Date : 2023-08-09 
+# -----------------------
+"""Data processing script for PUBHEALTH data
+"""
